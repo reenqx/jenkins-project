05fcd17f18b3a3251c9b9544177ddaa9043b8580
SEONMEEN <pasagone92@gmail.com>
2025-03-28 19:19:43 +0700

    music

--- netlify/functions/random-song.js
+++ netlify/functions/random-song.js
@@ -1,5 +1,5 @@
 const songs = [
-    { cover: "https://upload.wikimedia.org/wikipedia/commons/6/60/Charli_XCX_-_Brat_%28album_cover%29.png", title: "360", url: "https://example.com/song1.mp3" },
+    { cover: "https://upload.wikimedia.org/wikipedia/commons/6/60/Charli_XCX_-_Brat_%28album_cover%29.png", title: "360", url: "https://archive.org/details/09-so-i/01+360.wav" },
     { cover: "https://upload.wikimedia.org/wikipedia/commons/6/60/Charli_XCX_-_Brat_%28album_cover%29.png", title: "B2b", url: "https://example.com/song2.mp3" },
     { cover: "https://upload.wikimedia.org/wikipedia/commons/6/60/Charli_XCX_-_Brat_%28album_cover%29.png", title: "Apple", url: "https://example.com/song3.mp3" }
   ];
