a5ce574a603c3c7b7bb5235ed7ecd66624459fca
SEONMEEN <pasagone92@gmail.com>
2025-03-28 19:06:55 +0700

    brat

--- netlify/functions/random-song.js
+++ netlify/functions/random-song.js
@@ -1,8 +1,7 @@
-// netlify/functions/random-song.js
 const songs = [
-    { title: "เพลงที่ 1", artist: "ศิลปิน A", url: "https://example.com/song1.mp3" },
-    { title: "เพลงที่ 2", artist: "ศิลปิน B", url: "https://example.com/song2.mp3" },
-    { title: "เพลงที่ 3", artist: "ศิลปิน C", url: "https://example.com/song3.mp3" }
+    { cover: "https://upload.wikimedia.org/wikipedia/commons/6/60/Charli_XCX_-_Brat_%28album_cover%29.png", title: "360", url: "https://example.com/song1.mp3" },
+    { cover: "https://upload.wikimedia.org/wikipedia/commons/6/60/Charli_XCX_-_Brat_%28album_cover%29.png", title: "B2b", url: "https://example.com/song2.mp3" },
+    { cover: "https://upload.wikimedia.org/wikipedia/commons/6/60/Charli_XCX_-_Brat_%28album_cover%29.png", title: "Apple", url: "https://example.com/song3.mp3" }
   ];
   
   exports.handler = async (event, context) => {
